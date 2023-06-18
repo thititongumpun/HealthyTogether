52c672b68010307d147d2d2e0ad1b757fe1e1987
thititongumpun <thiti180536@gmail.com>
2023-06-19 00:37:20 +0700

    add middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,15 @@
 export { default } from "next-auth/middleware";
+import { getToken } from 'next-auth/jwt'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req })
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', req.nextUrl))
+  }
+}
 
 export const config = {
   matcher: ["/((?!register|api|login).*)"],
