7549c09073af0a71cf43f48f0bf87ec95abafc8f
thititongumpun <thiti180536@gmail.com>
2024-11-20 15:22:50 +0700

    add ai chat

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,9 @@
 import RetroGrid from "@/components/ui/retro-grid";
+import { CopilotKit } from "@copilotkit/react-core";
+import { CopilotPopup } from "@copilotkit/react-ui";
+import "@copilotkit/react-ui/styles.css";
 
-export default async function Page() {
+export default function Page() {
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
       <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold leading-none tracking-tighter text-transparent">
@@ -8,6 +11,9 @@ export default async function Page() {
       </span>
 
       <RetroGrid />
+      <CopilotKit runtimeUrl="/api/copilotkit">
+        <CopilotPopup />
+      </CopilotKit>
     </div>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -7,8 +7,6 @@ export default auth((req) => {
 })
 
 export const config = {
-  // matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/"],
-  // matcher: ["/"],
   matcher: [
     "/",
     "/dashboard/:path*",
